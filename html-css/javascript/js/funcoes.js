700df58033466a4fdb9255471376035b77038dac
Pedro Roque <dotjp1@gmail.com>
2023-11-10 16:38:02 -0300

    updating javascript

--- html-css/javascript/js/funcoes.js
+++ html-css/javascript/js/funcoes.js
@@ -35,8 +35,10 @@ function contagemRegressiva(numero){
     Formulário envio de dados para cálculo de média
 */
 
+const formulario1 = document.getElementById('formulario01');
 
-document.getElementById('formulario01').addEventListener('submit', function (evento) {
+if(formulario1)
+    formulario1.addEventListener('submit', function(evento){
 
     evento.preventDefault();
     evento.stopPropagation();
@@ -68,8 +70,8 @@ function validaCampo(elemento){
     elemento.addEventListener('focusout', function(event){
         event.preventDefault();
 
-        if(this.value == ''){
-            document.querySelector('.mensagem').innerHTML = 'verifique o preenchimento dos campos em vermelho';
+        if(this.value == ""){
+            document.querySelector('.mensagem').innerHTML = 'verifique o preenchimento dos campos em destaque';
             this.classList.add('erro');
             this.parentNode.classList.add('erro');
             return false;
@@ -86,7 +88,9 @@ function validaCampoNumerico(elemento){
     elemento.addEventListener('focusout', function(event){
         event.preventDefault();
 
-        if(this.value != '' && this.value.match(/[0-9]*/) && this.value >= 0 && this.value <= 10){
+        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value; 
+
+        if(numero != '' && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10){
             document.querySelector('.mensagem').innerHTML = '';
             this.classList.remove('erro');
             this.parentNode.classList.remove('erro');
@@ -96,11 +100,71 @@ function validaCampoNumerico(elemento){
             this.parentNode.classList.add('erro');
             return false;
         }
-    })
+    });
 }
+
+function validaEmail(elemento){
+    
+    elemento.addEventListener('focusout', function(event){
+        event.preventDefault();
+
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+
+        if(this.value.match(emailValido)){
+            document.querySelector('.mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else{
+            document.querySelector('.mensagem').innerHTML = 'verifique o preenchimento dos campos em destaque';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+function validaCep(elemento){
+
+    elemento.addEventListener('focusout', function(event){
+        event.preventDefault();
+
+        const regexCep = /^\d{5}-\d{3}$/;
+
+        if(this.value.match(regexCep)){
+            document.querySelector('.mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else{
+            document.querySelector('.mensagem').innerHTML = 'Cep inválido. O formato deve ser XXXXX-XXX. ';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+function formatarCEP() {
+    const cepInput = document.getElementById('cepInput');
+
+    // Remove caracteres não numéricos
+    const cepFormatado = cepInput.value.replace(/\D/g, '');
+
+    // Adiciona o hífen se necessário
+    if (cepFormatado.length >= 5) {
+      cepInput.value = cepFormatado.slice(0, 5) + '-' + cepFormatado.slice(5, 8);
+    } else {
+      cepInput.value = cepFormatado;
+    }
+}
+
+document.getElementById('cepInput').addEventListener('input', function(event) {
+    formatarCEP(); // Chama a função para formatar o CEP enquanto o usuário digita
+});
 
 let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
 let camposNumericos = document.querySelectorAll('input.numero');
+let camposEmail = document.querySelectorAll('input.email');
+let camposCep = document.querySelectorAll('input.cep')
 
 for(let emFoco of camposObrigatorios){
     validaCampo(emFoco);
@@ -108,4 +172,12 @@ for(let emFoco of camposObrigatorios){
 
 for(let emFoco of camposNumericos){
     validaCampoNumerico(emFoco);
+}
+
+for(let emFoco of camposEmail){
+    validaEmail(emFoco);
+}
+
+for(let emFoco of camposCep){
+    validaCep(emFoco);
 }
